2e838f9c6239369aaac3f914d778b582b02baf0d
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-02-07 13:51:14 +0530

    regsitration and login completed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -8,17 +8,44 @@ cloudinary.config({
   api_secret: "Ey9rv2Re9rvpU7QRq_Q4235df7I", // replace with your cloudinary API Secret
 });
 
-export const uploadOnCloudinary = async (localpath) => {
+// export const uploadOnCloudinary = async (localpath) => {
+//   try {
+//     if (!localpath) throw new Error("Local path is missing");
+//     const response = await cloudinary.uploader.upload_stream(localpath, {
+//       resource_type: "auto",
+//     });
+//     fs.unlinkSync(localpath);
+//     return response;
+//   } catch (error) {
+//     console.error("Error uploading to Cloudinary:", error.message);
+//     fs.unlinkSync(localpath);
+//     throw new Error("Error uploading to Cloudinary");
+//   }
+// };
+
+export const uploadOnCloudinary = async (fileBuffer) => {
   try {
-    if (!localpath) throw new Error("Local path is missing");
-    const response = await cloudinary.uploader.upload(localpath, {
-      resource_type: "auto",
-    });
-    fs.unlinkSync(localpath);
+    if (!fileBuffer) throw new Error("File buffer is missing");
+
+    const response = await cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+        },
+        (error, result) => {
+          if (error) {
+            console.error("Error uploading to Cloudinary:", error.message);
+            throw new Error("Error uploading to Cloudinary");
+          }
+          console.log("Cloudinary Response:", result);
+          return result;
+        }
+      )
+      .end(fileBuffer);
+
     return response;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error.message);
-    fs.unlinkSync(localpath);
-    throw new Error("Error uploading to Cloudinary");
+    throw error;
   }
 };
