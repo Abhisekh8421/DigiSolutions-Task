5f56159ca9fe67ccbfb073bcd15370d216933528
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-02-07 13:25:32 +0530

    regsitration and login completed

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,8 +1,25 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
+
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public");
+    const uploadPath = path.join(__dirname, "public");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
