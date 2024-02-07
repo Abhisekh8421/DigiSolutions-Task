90f0b5e24780244d802019d1d372335cdd9fde36
Suru Abhisekh <abhisekhsuru@gmail.com>
2024-02-07 14:09:31 +0530

    regsitration and login completed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -8,32 +8,17 @@ cloudinary.config({
   api_secret: "Ey9rv2Re9rvpU7QRq_Q4235df7I", // replace with your cloudinary API Secret
 });
 
-// export const uploadOnCloudinary = async (localpath) => {
-//   try {
-//     if (!localpath) throw new Error("Local path is missing");
-//     const response = await cloudinary.uploader.upload_stream(localpath, {
-//       resource_type: "auto",
-//     });
-//     fs.unlinkSync(localpath);
-//     return response;
-//   } catch (error) {
-//     console.error("Error uploading to Cloudinary:", error.message);
-//     fs.unlinkSync(localpath);
-//     throw new Error("Error uploading to Cloudinary");
-//   }
-// };
-
-export const uploadOnCloudinary = async (fileBuffer) => {
+export const uploadOnCloudinary = async (localpath) => {
   try {
-    if (!fileBuffer) throw new Error("File buffer is missing");
-
-    const response = await cloudinary.uploader.upload(fileBuffer, {
+    if (!localpath) throw new Error("Local path is missing");
+    const response = await cloudinary.uploader.upload_stream(localpath, {
       resource_type: "auto",
     });
-
+    fs.unlinkSync(localpath);
     return response;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error.message);
-    throw error;
+    fs.unlinkSync(localpath);
+    throw new Error("Error uploading to Cloudinary");
   }
 };
